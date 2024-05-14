bd7f2619fad8d6bd28b9f4e969a91c99cd867f07
franclocat <148209747+franclocat@users.noreply.github.com>
2024-05-14 23:30:48 +0200

    Working "quizezs" backend

--- src/main/java/com/example/WebQuizService/Completion.java
+++ src/main/java/com/example/WebQuizService/Completion.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
